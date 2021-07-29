63a8e9e5cba9234da19cac16ab28b48da0cff7a6
Boyuan Ma <maboyuan@maboyuandeMacBook-Pro.local>
2021-07-29 21:39:33 +0100

    Add about page and media

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # class Category(models.Model):
 #     name = models.CharField(max_length=128, unique=True)
     
-#     def __str__(self):  # For Python 2, use __unicode__ too
+#     def __str__(self):
 #         return self.name
 
 
@@ -13,6 +13,6 @@ from django.db import models
 #     url = models.URLField()
 #     views = models.IntegerField(default=0)
 
-#     def __str__(self):  # For Python 2, use __unicode__ too
+#     def __str__(self):
 #         return self.title
 
